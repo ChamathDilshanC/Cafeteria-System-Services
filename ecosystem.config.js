e7b8e28c9873b54e7055a6aa7e1229a6390675b8
ChamathDilshanC <dilshancolonne123@gmail.com>
2026-03-22 11:52:10 +0530

    Add PM2 logging config for services
    Add per-service PM2 logging settings (out_file, error_file) and enable merge_logs and time for user-, menu-, order- and kitchen-service so stdout/stderr are persisted to ./logs/*.log with timestamps. Also adjust trailing commas in env objects for consistency. These changes improve debugging and log aggregation for running services.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,50 +5,66 @@ module.exports = {
       script: 'java',
       args: ['-jar', 'user-service/target/user-service-1.0.0.jar'],
       cwd: './',
+      out_file: './logs/user-service-out.log',
+      error_file: './logs/user-service-error.log',
+      merge_logs: true,
+      time: true,
       env: {
         SERVER_PORT: 8081,
         SPRING_DATASOURCE_URL: 'jdbc:mysql://localhost:3306/cafeteria_users',
         CONFIG_SERVER_URI: 'http://localhost:8888',
-        EUREKA_URI: 'http://localhost:8761/eureka'
-      }
+        EUREKA_URI: 'http://localhost:8761/eureka',
+      },
     },
     {
       name: 'menu-service',
       script: 'java',
       args: ['-jar', 'menu-service/target/menu-service-1.0.0.jar'],
       cwd: './',
+      out_file: './logs/menu-service-out.log',
+      error_file: './logs/menu-service-error.log',
+      merge_logs: true,
+      time: true,
       env: {
         SERVER_PORT: 8082,
         SPRING_DATASOURCE_URL: 'jdbc:mysql://localhost:3306/cafeteria_menu',
         GCS_BUCKET_NAME: 'cafeteria-menu-images',
         CONFIG_SERVER_URI: 'http://localhost:8888',
-        EUREKA_URI: 'http://localhost:8761/eureka'
-      }
+        EUREKA_URI: 'http://localhost:8761/eureka',
+      },
     },
     {
       name: 'order-service',
       script: 'java',
       args: ['-jar', 'order-service/target/order-service-1.0.0.jar'],
       cwd: './',
+      out_file: './logs/order-service-out.log',
+      error_file: './logs/order-service-error.log',
+      merge_logs: true,
+      time: true,
       env: {
         SERVER_PORT: 8083,
         SPRING_DATASOURCE_URL: 'jdbc:mysql://localhost:3306/cafeteria_orders',
         CONFIG_SERVER_URI: 'http://localhost:8888',
-        EUREKA_URI: 'http://localhost:8761/eureka'
-      }
+        EUREKA_URI: 'http://localhost:8761/eureka',
+      },
     },
     {
       name: 'kitchen-service',
       script: 'java',
       args: ['-jar', 'kitchen-service/target/kitchen-service-1.0.0.jar'],
       cwd: './',
+      out_file: './logs/kitchen-service-out.log',
+      error_file: './logs/kitchen-service-error.log',
+      merge_logs: true,
+      time: true,
       env: {
         SERVER_PORT: 8084,
         MONGODB_HOST: 'localhost',
         MONGODB_PORT: 27017,
         CONFIG_SERVER_URI: 'http://localhost:8888',
-        EUREKA_URI: 'http://localhost:8761/eureka'
-      }
-    }
-  ]
+        EUREKA_URI: 'http://localhost:8761/eureka',
+      },
+    },
+  ],
 };
