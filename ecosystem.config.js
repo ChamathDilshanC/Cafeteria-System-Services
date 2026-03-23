1e415a166db546f31f3f952df71f580710d2da65
ChamathDilshanC <dilshancolonne123@gmail.com>
2026-03-23 22:20:42 +0530

    Update ecosystem config: run jars & scale
    Simplify PM2 app entries by replacing separate 'java' + args with a single "java -jar <service>.jar" script, remove per-app cwd and env_production settings, and increase instances from 1 to 2 for user/menu/order/kitchen services. Also update submodule pointers for kitchen-service, menu-service, order-service, and user-service to new commits.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,52 +1,28 @@
 module.exports = {
   apps: [
     {
-      name: 'user-service',
-      script: 'java',
-      args: ['-jar', '-Dserver.port=0', 'user-service/target/user-service-1.0.0.jar'],
-      instances: 1,
-      cwd: './',
-      log_file: './logs/user-service.log',
-      env_production: {
-        CONFIG_SERVER_URI: 'http://config.platform:9000',
-        EUREKA_URI: 'http://vm-node-a.platform:8761/eureka/,http://vm-node-b.platform:8761/eureka/,http://vm-node-c.platform:8761/eureka/'
-      }
+      name: "user-service",
+      script: "java -jar user-service/target/user-service-1.0.0.jar",
+      log_file: "./logs/user-service.log",
+      instances: 2
     },
     {
-      name: 'menu-service',
-      script: 'java',
-      args: ['-jar', '-Dserver.port=0', 'menu-service/target/menu-service-1.0.0.jar'],
-      instances: 1,
-      cwd: './',
-      log_file: './logs/menu-service.log',
-      env_production: {
-        CONFIG_SERVER_URI: 'http://config.platform:9000',
-        EUREKA_URI: 'http://vm-node-a.platform:8761/eureka/,http://vm-node-b.platform:8761/eureka/,http://vm-node-c.platform:8761/eureka/'
-      }
+      name: "menu-service",
+      script: "java -jar menu-service/target/menu-service-1.0.0.jar",
+      log_file: "./logs/menu-service.log",
+      instances: 2
     },
     {
-      name: 'order-service',
-      script: 'java',
-      args: ['-jar', '-Dserver.port=0', 'order-service/target/order-service-1.0.0.jar'],
-      instances: 1,
-      cwd: './',
-      log_file: './logs/order-service.log',
-      env_production: {
-        CONFIG_SERVER_URI: 'http://config.platform:9000',
-        EUREKA_URI: 'http://vm-node-a.platform:8761/eureka/,http://vm-node-b.platform:8761/eureka/,http://vm-node-c.platform:8761/eureka/'
-      }
+      name: "order-service",
+      script: "java -jar order-service/target/order-service-1.0.0.jar",
+      log_file: "./logs/order-service.log",
+      instances: 2
     },
     {
-      name: 'kitchen-service',
-      script: 'java',
-      args: ['-jar', '-Dserver.port=0', 'kitchen-service/target/kitchen-service-1.0.0.jar'],
-      instances: 1,
-      cwd: './',
-      log_file: './logs/kitchen-service.log',
-      env_production: {
-        CONFIG_SERVER_URI: 'http://config.platform:9000',
-        EUREKA_URI: 'http://vm-node-a.platform:8761/eureka/,http://vm-node-b.platform:8761/eureka/,http://vm-node-c.platform:8761/eureka/'
-      }
-    },
-  ],
+      name: "kitchen-service",
+      script: "java -jar kitchen-service/target/kitchen-service-1.0.0.jar",
+      log_file: "./logs/kitchen-service.log",
+      instances: 2
+    }
+  ]
 };
