8e85046086dc13ec96901093870417c3b8b8f52b
ChamathDilshanC <dilshancolonne123@gmail.com>
2026-03-26 20:51:22 +0530

    Force Config Server import in PM2 apps
    Change PM2 app entries for user, menu, order and kitchen services to use 'java' with args that include '--spring.config.import=configserver:http://config.platform:9000' so they load configuration from the central Config Server. Add autorestart: true for kitchen-service and update the kitchen-service submodule commit reference. Minor comment cleanup in ecosystem.config.js.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   apps: [
-    // auth proxy server start first
     {
       name: 'cloud-sql-auth-proxy',
       script: './cloud-sql-proxy',
@@ -11,27 +10,33 @@ module.exports = {
     },
     {
       name: 'user-service',
-      script: 'java -jar user-service/target/user-service-1.0.0.jar',
+      script: 'java',
+      args: '-jar user-service/target/user-service-1.0.0.jar --spring.config.import=configserver:http://config.platform:9000',
       log_file: './logs/user-service.log',
       instances: 2,
     },
     {
       name: 'menu-service',
-      script: 'java -jar menu-service/target/menu-service-1.0.0.jar',
+      script: 'java',
+      args: '-jar menu-service/target/menu-service-1.0.0.jar --spring.config.import=configserver:http://config.platform:9000',
       log_file: './logs/menu-service.log',
       instances: 2,
     },
     {
       name: 'order-service',
-      script: 'java -jar order-service/target/order-service-1.0.0.jar',
+      script: 'java',
+      args: '-jar order-service/target/order-service-1.0.0.jar --spring.config.import=configserver:http://config.platform:9000',
       log_file: './logs/order-service.log',
       instances: 2,
     },
     {
       name: 'kitchen-service',
-      script: 'java -jar kitchen-service/target/kitchen-service-1.0.0.jar',
+      script: 'java',
+      // මෙතන තමයි වැදගත්ම දේ - Config Server එක force කරනවා
+      args: '-jar kitchen-service/target/kitchen-service-1.0.0.jar --spring.config.import=configserver:http://config.platform:9000',
       log_file: './logs/kitchen-service.log',
       instances: 2,
+      autorestart: true,
     },
   ],
 };
