510a3ae37afe8803fd06dd24fdca1bbbbbc0154b
ChamathDilshanC <dilshancolonne123@gmail.com>
2026-03-27 16:09:25 +0530

    Use configserver IP and enable autorestart
    Update services to load config from configserver at 10.148.0.6:9000 instead of config.platform. Enable autorestart for services and standardize log_file for kitchen-service. Increase order-service instances from 1 to 2. Remove the hard-coded MongoDB URI env from kitchen-service so configuration is sourced from the config server.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,32 +11,29 @@ module.exports = {
     {
       name: 'user-service',
       script: 'java',
-      args: '-jar user-service/target/user-service-1.0.0.jar --spring.config.import=configserver:http://config.platform:9000',
+      args: '-jar user-service/target/user-service-1.0.0.jar --spring.config.import=configserver:http://10.148.0.6:9000',
       log_file: './logs/user-service.log',
       instances: 2,
     },
     {
       name: 'menu-service',
       script: 'java',
-      args: '-jar menu-service/target/menu-service-1.0.0.jar --spring.config.import=configserver:http://config.platform:9000',
+      args: '-jar menu-service/target/menu-service-1.0.0.jar --spring.config.import=configserver:http://10.148.0.6:9000',
       log_file: './logs/menu-service.log',
       instances: 2,
     },
     {
       name: 'order-service',
       script: 'java',
-      args: '-jar order-service/target/order-service-1.0.0.jar --spring.config.import=configserver:http://config.platform:9000',
+      args: '-jar order-service/target/order-service-1.0.0.jar --spring.config.import=configserver:http://10.148.0.6:9000',
       log_file: './logs/order-service.log',
-      instances: 1,
+      instances: 2,
     },
     {
       name: 'kitchen-service',
       script: 'java',
-      args: '-jar kitchen-service/target/kitchen-service-1.0.0.jar',
-      env: {
-        SPRING_DATA_MONGODB_URI:
-          'mongodb://root:JRTgq5KibyHckge0t5SWLXMgjzdH5IJk6ozSAx7k8pYrwoXp@f2e45088-9d0e-4153-9ea7-e017549f617d.asia-southeast1.firestore.goog:443/cafeteria-firestore-mongo?loadBalanced=true&tls=true&authMechanism=SCRAM-SHA-256&retryWrites=false',
-      },
+      args: '-jar kitchen-service/target/kitchen-service-1.0.0.jar --spring.config.import=configserver:http://10.148.0.6:9000',
+      log_file: './logs/kitchen-service.log',
       instances: 2,
       autorestart: true,
     },
